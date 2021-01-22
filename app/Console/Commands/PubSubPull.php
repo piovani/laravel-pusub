c6f7009622f0ad7a5a14a0e2221723f04c4d7a28
Allison Piovani <apiovani25@gmail.com>
2021-01-22 18:09:30 -0300

    Fixing use case

--- app/Console/Commands/PubSubPull.php
+++ app/Console/Commands/PubSubPull.php
@@ -5,17 +5,25 @@ namespace App\Console\Commands;
 use Google\Cloud\PubSub\PubSubClient;
 use Illuminate\Console\Command;
 
-class PubSub extends Command
+class PubSubPull extends Command
 {
     protected $signature = 'pubsub:pull
                                     {topic : Name of the topic that should be picked up the messages}
                                     {--no-delete : Do not delete the message after reading}';
 
-    protected $description = 'Command description';
+    protected $description = 'Retrieve messages from the informed topic';
 
     public function handle(PubSubClient $pubSub)
     {
-        $subscription = $pubSub->subscription($this->argument('topic'));
+        $nameTopic = $this->argument('topic');
+        $topic = $pubSub->topic($nameTopic);
+
+        if (!$topic->exists()) {
+            $topic->create();
+            $subscription = $topic->subscribe($nameTopic);
+        } else {
+            $subscription = $topic->subscription($nameTopic);
+        }
 
         while (true) {
             $messages = $subscription->pull(['returnImmediately' => true]);
